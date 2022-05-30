a347de39dfdb4b949a81075543bb5a5fc7b36eab
Sjoerd de Vries <vriesdesjoerd@gmail.com>
2022-05-30 10:40:26 +0200

    Update install.sh

--- install.sh
+++ install.sh
@@ -10,3 +10,8 @@ sudo apt update && \
 sudo apt install docker-ce
 
 sudo usermod -aG docker ${USER}
+
+# Docker-compose
+mkdir -p ~/.docker/cli-plugins/
+curl -SL https://github.com/docker/compose/releases/download/v2.3.3/docker-compose-linux-x86_64 -o ~/.docker/cli-plugins/docker-compose
+chmod +x ~/.docker/cli-plugins/docker-compose
